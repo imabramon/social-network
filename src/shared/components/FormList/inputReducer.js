544f621a415a67ed634966a7be4adc97e6cd3fdc
im_abramon <shelopugin.vlad@yandex.ru>
2024-04-02 09:57:34 +0300

    редьюсер для динамического списка

--- src/shared/components/FormList/inputReducer.js
+++ src/shared/components/FormList/inputReducer.js
@@ -1,39 +1,37 @@
-import { InputActionsType } from "./inputActions";
+import { InputActionsType } from './inputActions';
 
-export const inputsReducer = (state = [], action)=>{
+export const inputsReducer = (state = [], action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case InputActionsType.add: {
+      const { value } = payload;
 
-	const {type, payload} = action;
-	switch (type) {
-		case InputActionsType.add:{
-			const {value} = payload;
+      return [...state, { value }];
+    }
 
-			return [...state, {value}]
-		}
-			
-		case InputActionsType.remove:
-			const {index} = payload;
-			const newState = state.reduce((arr, elem, elemIndex) => {
-				if(elemIndex === index){
-					return [...arr]
-				}
+    case InputActionsType.remove:
+      const { index } = payload;
+      const newState = state.reduce((arr, elem, elemIndex) => {
+        if (elemIndex === index) {
+          return [...arr];
+        }
 
-				return [...arr, elem]
-			}, [])
-    
-			return newState;
-		case InputActionsType.change:{
-			const {index, value} = payload;
-			const newState = state.reduce((arr, elem, elemIndex) => {
-				if(elemIndex === index){
-					return [...arr, {value}]
-				}
+        return [...arr, elem];
+      }, []);
 
-				return [...arr, elem]
-			}, [])
-			return newState;
-		}
-		default:
-			return state;
-	}
+      return newState;
+    case InputActionsType.change: {
+      const { index, value } = payload;
+      const newState = state.reduce((arr, elem, elemIndex) => {
+        if (elemIndex === index) {
+          return [...arr, { value }];
+        }
 
-}
+        return [...arr, elem];
+      }, []);
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
