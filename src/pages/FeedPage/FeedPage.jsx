168f77c3b96a5e9c8262d368fcd4f8040644d2cb
im_abramon <shelopugin.vlad@yandex.ru>
2024-04-03 17:56:28 +0300

    Фид

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -1,11 +1,25 @@
 'use client';
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import PostCard from '../../components/PostCard/PostCard';
+import { VStack } from '../../shared/ui';
 
 const FeedPage = () => {
   useEffect(() => {}, []);
-  return <FeedPageStl>FeedPage works!</FeedPageStl>;
+  return (
+    <FeedPageStl>
+      <VStack $gap="26px">
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </VStack>
+    </FeedPageStl>
+  );
 };
 
-export const FeedPageStl = styled.div``;
+export const FeedPageStl = styled.div`
+  width: 938px;
+  height: fit-content;
+  margin-top: 30px;
+`;
 export default FeedPage;
