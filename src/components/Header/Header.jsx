07f91b2e1ab8e61bbefcfb95521c164c467207d5
im_abramon <shelopugin.vlad@yandex.ru>
2024-04-05 15:26:35 +0300

    Хедер

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,56 @@
-"use client";
+'use client';
 import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
+import { HStack, NameTitle, Avatar } from '../../shared/ui';
+import Button from '../../shared/components/Button';
+import withLoggedSwitch from '../../shared/hoc/withLoggedSwitch';
+import { useSelector } from 'react-redux';
 
-const Header = ({}) => {
-	return (
-		<HeaderStl>
- 			Header works!
- 		</HeaderStl>
-	);
+const ButtonNoBorder = Button.Normal.NoBorder.Neutral.Fit;
+const ButtonHighlighted = Button.Normal.Highlighted.Action.Fit;
+const ButtonHighlightedSmall = Button.Small.Highlighted.Action.Fit;
+const ButtonHighlightedNeutral = Button.Normal.Highlighted.Neutral.Fit;
+
+const UnloggedSide = () => (
+  <HStack width="fit-content" $gap="20px">
+    <ButtonNoBorder>Sign In</ButtonNoBorder>
+    <ButtonHighlighted>Sign Up</ButtonHighlighted>
+  </HStack>
+);
+
+const UserData = () => {
+  const name = useSelector((state) => state.userData.name);
+  const avatarUrl = useSelector((state) => state.userData.avatarUrl);
+
+  return (
+    <HStack width="fit-content" $gap="15px" $alignItems="center">
+      <NameTitle>{name}</NameTitle>
+      <Avatar src={avatarUrl} />
+    </HStack>
+  );
 };
 
-export const HeaderStl = styled.div``;
-Header.propTypes = {};
+const LoggedSide = () => (
+  <HStack width="fit-content" $gap="20px" $alignItems="center">
+    <ButtonHighlightedSmall>Create article</ButtonHighlightedSmall>
+    <UserData />
+    <ButtonHighlightedNeutral>Log Out</ButtonHighlightedNeutral>
+  </HStack>
+);
+
+const Header = ({}) => {
+  return (
+    <HeaderStl>
+      <HStack $justifyContent="space-between" $alignItems="center">
+        <span>Realworld Blog</span>
+        {withLoggedSwitch(LoggedSide, UnloggedSide)}
+      </HStack>
+    </HeaderStl>
+  );
+};
+
+export const HeaderStl = styled.div`
+  padding: 16px 25px;
+`;
 
 export default Header;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,10 @@
 const initialState = {
   logged: false,
+  userData: {
+    name: 'abramon',
+    avatarUrl:
+      'https://sun9-60.userapi.com/s/v1/ig2/nHV2SqpSQ7Q7BkqK8_cWXYwZf4e4weIdpe1DGcW9_e4SkyK0Rw-acA4baUuUMLu_o8imF5xwLf0tD5hfH7zxHAQx.jpg?size=50x50&quality=95&crop=84,40,509,509&ava=1',
+  },
 };
 
 export const reducer = (state = initialState, action) => {
