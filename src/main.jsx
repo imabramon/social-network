35aaafebfcd7f98b8adef9c32a829d96a11652e4
im_abramon <shelopugin.vlad@yandex.ru>
2024-05-17 11:28:08 +0300

    Тест нотификейшен для прода

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,28 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { QueryClientProvider, QueryClient } from 'react-query'
 import { PersistGate } from 'redux-persist/integration/react'
+import { ToastContainer } from 'react-toastify'
 import App from './components/App.jsx'
 import { store, persistor } from './store/store'
+import 'react-toastify/dist/ReactToastify.css'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <PersistGate loading={<div>loading...</div>} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
-    </QueryClientProvider>
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <div>
+        <ToastContainer />
+      </div>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <PersistGate loading={<div>loading...</div>} persistor={persistor}>
+            {/* <App /> */}
+
+            <App />
+          </PersistGate>
+        </Provider>
+      </QueryClientProvider>
+    </React.StrictMode>
+  </>
 )
--- src/pages/EditArticlePage/EditArticlePage.jsx
+++ src/pages/EditArticlePage/EditArticlePage.jsx
@@ -1,9 +1,11 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { useNavigate, useParams } from 'react-router'
 import { useQuery } from 'react-query'
+import { useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
 import ArticleForm from '../../forms/ArticleForm'
 import { loadArticle, editArticle } from '../../api'
 import { PagePath } from '../../consts/pagePath'
@@ -11,6 +13,26 @@ import { PagePath } from '../../consts/pagePath'
 function EditArticlePage() {
   const { id } = useParams()
   const navigate = useNavigate()
+  const [isLogged, username] = useSelector((state) => [
+    state.userData.logged,
+    state.userData.username,
+  ])
+
+  const goBackWithNotify = (msg) => {
+    setTimeout(
+      () =>
+        toast(msg, {
+          autoClose: 5000,
+        }),
+      1
+    )
+    navigate(-1)
+    // ;(async () => {
+    //   toast(msg, {
+    //     autoClose: 5000,
+    //   })
+    // })()
+  }
 
   const {
     data: articleData,
@@ -20,6 +42,19 @@ function EditArticlePage() {
     queryKey: ['article', id],
     queryFn: loadArticle,
   })
+
+  useEffect(() => {
+    if (!isLogged) {
+      goBackWithNotify(
+        'Вы не можете редактировать статью так как не авторизованы'
+      )
+      return
+    }
+
+    if (!isLoading && username !== articleData.userInfo.name) {
+      goBackWithNotify('Вы не можете редактировать статью другого пользователя')
+    }
+  }, [isLogged, username, articleData])
 
   return (
     <EditArticlePageStl>
